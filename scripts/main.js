42cbaf9696b34565d52ee4bd4275e9c072c94b7d
CZR <caizirong09@gmail.com>
2021-03-19 19:21:10 +0100

    context changed

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,16 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-        myImage.setAttribute('src','images/1.png');
+    if(mySrc === 'images/stand.png') {
+        myImage.setAttribute('src','images/pow.png');
     } else {
-        myImage.setAttribute('src','images/firefox-icon.png');
+        myImage.setAttribute('src','images/stand.png');
     }
 }
 
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1')
+let myHeading = document.querySelector('h2')
+let myButton = document.querySelector('button')
 
 function setUserName() {
     let myName = prompt('Please enter your name.');
@@ -20,7 +20,7 @@ function setUserName() {
         setUserName();
     } else {
         localStorage.setItem('name', myName);
-        myHeading.textContent = 'Mozilla is cool,' + myName;
+        myHeading.textContent = 'Dear ' + myName + ',';
     }
 }
 
@@ -28,10 +28,9 @@ if(!localStorage.getItem('name')) {
     setUserName();
 } else {
     let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool,' + storedName;
+    myHeading.textContent = 'Dear ' + storedName + ',';
 
 }
-
 
 myButton.onclick = function() {
     setUserName();
